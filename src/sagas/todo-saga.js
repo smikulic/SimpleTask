4f749125fb25cb75f45aaf5ab790c0ae24c63a38
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2018-07-12 17:54:14 -0400

    Add saga specs

--- src/sagas/todo-saga.js
+++ src/sagas/todo-saga.js
@@ -16,10 +16,9 @@ import { responseTodosMock } from '../redux-test-helper';
 export function* indexTodo() {
   try {
     // Mock response - later on can be replaced with actual calls
-     const response = JSON.parse(window.localStorage.getItem('simple_todos'));
-      yield put(todoIndexLoadSuccess(response.data));
+    const response = JSON.parse(window.localStorage.getItem('simple_todos'));
+    yield put(todoIndexLoadSuccess(response.data));
   } catch (e) {
-    // FOR MOCK DATA IT WILL ALWAYS RETURN SUCCESSFULLY
     yield put(todoIndexLoadFail(e.message));
   }
 }
@@ -32,14 +31,16 @@ export function* indexTodoOnEnter() {
 
 // CREATE
 export function* createTodo(payload) {
-  try {
-    let currentTodos = JSON.parse(window.localStorage.getItem('simple_todos'));
-    currentTodos.data.push(payload);
-    window.localStorage.setItem('simple_todos', JSON.stringify(currentTodos));
-    const response = currentTodos;
-    yield put(todoIndexLoadSuccess(response.data));
-  } catch (e) {
-    yield put(todoCreateFail(e.message));
+  if (payload) {
+    try {
+      let currentTodos = JSON.parse(window.localStorage.getItem('simple_todos'));
+      currentTodos.data.push(payload);
+      window.localStorage.setItem('simple_todos', JSON.stringify(currentTodos));
+      const response = currentTodos;
+      yield put(todoIndexLoadSuccess(response.data));
+    } catch (e) {
+      yield put(todoCreateFail(e.message));
+    }
   }
 }
 
@@ -63,7 +64,6 @@ export function* updateTodo(payload) {
       todo.id === payload.id ? updatedTodo = payload : updatedTodo = todo;
       updatedTodos.data.push(updatedTodo);
     });
-
     window.localStorage.setItem('simple_todos', JSON.stringify(updatedTodos));
     const response = updatedTodos;
     yield put(todoIndexLoadSuccess(response.data));
--- src/sagas/todo-saga.test.js
+++ src/sagas/todo-saga.test.js
@@ -1,22 +1,35 @@
-import { put } from 'redux-saga/effects';
+import { put, call } from 'redux-saga/effects';
 import {
   todosMock,
   responseTodosMock,
 } from '../redux-test-helper';
 import {
   todoIndexLoadSuccess,
-  // todoIndexLoadFail,
+  todoIndexLoadFail,
+  todoCreateFail,
+  todoUpdateFail,
 } from '../redux/actions/todo-actions';
-import { indexTodo } from './todo-saga';
+import {
+  indexTodo,
+  createTodo,
+  updateTodo,
+} from './todo-saga';
 
 let actualYield;
 let expectedYield;
 
 describe('todo saga', () => {
+
   // INDEX
   describe('indexTodo ', () => {
     describe('when data is fetched successfully', () => {
       let iterator = indexTodo();
+      beforeAll(() => {
+        window.localStorage = {
+          getItem: () => JSON.stringify(responseTodosMock)
+        };
+      });
+
       it('stores todos list in the state', () => {
         actualYield = iterator.next(responseTodosMock).value;
         expectedYield = put(todoIndexLoadSuccess(todosMock));
@@ -27,16 +40,142 @@ describe('todo saga', () => {
       });
     });
     describe('when data is not fetched successfully', () => {
-      // FOR MOCK DATA IT WILL ALWAYS RETURN SUCCESSFULLY
-      //let iterator = indexTodo();
-      // it('triggers todoIndexLoadFail action', () => {
-      //   actualYield = iterator.next().value;
-      //   expectedYield = put(todoIndexLoadFail(`Cannot read property 'data' of undefined`));
-      //   expect(actualYield).toEqual(expectedYield);
-      // });
-      // it('it should be done', () => {
-      //   expect(iterator.next().done).toEqual(true);
-      // });
+      let iterator = indexTodo();
+      beforeAll(() => {
+        window.localStorage = {
+          getItem: () => null
+        };
+      });
+
+      it('triggers todoIndexLoadFail action', () => {
+        actualYield = iterator.next().value;
+        expectedYield = put(todoIndexLoadFail(`Cannot read property 'data' of null`));
+        expect(actualYield).toEqual(expectedYield);
+      });
+      it('it should be done', () => {
+        expect(iterator.next().done).toEqual(true);
+      });
+    });
+  });
+
+  // CREATE
+  describe('createTodo ', () => {
+    let newTodo = {
+      id: 123,
+      name: 'Test',
+      completed: false,
+    };
+    let updatedTodos = {
+      data: [
+        ...responseTodosMock.data,
+        newTodo,
+      ],
+    };
+
+    describe('when todo is created successfully', () => {
+      let iterator = createTodo(newTodo);
+      beforeAll(() => {
+        window.localStorage = {
+          getItem: () => JSON.stringify(responseTodosMock),
+          setItem: () => JSON.stringify(updatedTodos),
+        };
+      });
+
+      it('creates new todo', () => {
+        actualYield = iterator.next(updatedTodos).value;
+        expectedYield = put(todoIndexLoadSuccess(updatedTodos.data));
+        expect(actualYield).toEqual(expectedYield);
+      });
+      it('it should be done', () => {
+        expect(iterator.next().done).toEqual(true);
+      });
+    });
+    describe('when todo is not created successfully', () => {
+      let iterator = createTodo(newTodo);
+      beforeAll(() => {
+        window.localStorage = {
+          getItem: () => null,
+          setItem: () => null,
+        };
+      });
+
+      it('triggers todoCreateFail action', () => {
+        actualYield = iterator.next().value;
+        expectedYield = put(todoCreateFail(`Cannot read property 'data' of null`));
+        expect(actualYield).toEqual(expectedYield);
+      });
+      it('it should be done', () => {
+        expect(iterator.next().done).toEqual(true);
+      });
+    });
+    describe('when there is no create payload', () => {
+      let iterator = createTodo();
+      it('it should be done', () => {
+        expect(iterator.next().done).toEqual(true);
+      });
+    });
+  });
+
+  // UPDATE
+  describe('updateTodo ', () => {
+    let updatedTodo = {
+      id: 2,
+      name: 'Test',
+      completed: false,
+    };
+    let updatedTodos = {
+      data: [
+        responseTodosMock.data[0],
+        updatedTodo,
+      ],
+    };
+
+    describe('when todo is updated successfully', () => {
+      let iterator = updateTodo(updatedTodo);
+      beforeAll(() => {
+        window.localStorage = {
+          getItem: () => JSON.stringify(updatedTodos),
+          setItem: () => JSON.stringify(updatedTodos),
+        };
+      });
+
+      it('updates new todo', () => {
+        actualYield = iterator.next(updatedTodos).value;
+        expectedYield = put(todoIndexLoadSuccess(updatedTodos.data));
+        expect(actualYield).toEqual(expectedYield);
+      });
+      it('it should be done', () => {
+        expect(iterator.next().done).toEqual(true);
+      });
+    });
+    describe('when todo is not updated successfully', () => {
+      let iterator = updateTodo(updatedTodo);
+      beforeAll(() => {
+        window.localStorage = {
+          getItem: () => null,
+          setItem: () => null,
+        };
+      });
+
+      it('triggers todoUpdateFail action', () => {
+        actualYield = iterator.next().value;
+        expectedYield = put(todoUpdateFail(`Cannot read property 'data' of null`));
+        expect(actualYield).toEqual(expectedYield);
+      });
+      it('it should be done', () => {
+        expect(iterator.next().done).toEqual(true);
+      });
+    });
+    describe('when there is no update payload', () => {
+      let iterator = updateTodo();
+      it('triggers todoUpdateFail action', () => {
+        actualYield = iterator.next().value;
+        expectedYield = put(todoUpdateFail(`Cannot read property 'data' of null`));
+        expect(actualYield).toEqual(expectedYield);
+      });
+      it('it should be done', () => {
+        expect(iterator.next().done).toEqual(true);
+      });
     });
   });
 });
